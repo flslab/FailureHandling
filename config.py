c7677e9d9a546c79dd986e69dd1337a23f28c35d
Shuqin <shuqinzhu37@gmail.com>
2023-08-14 13:43:14 -0700

    fix wrong file name

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     SHAPE = 'racecar'
     RESULTS_PATH = 'results'
     DEBUG = True
-    FILE_NAME_KEYS = [('DISPATCHERS', 'D'), ('DISPATCH_RATE', 'R'), ('FAILURE_TIMEOUT', 'T'), ('MAX_SPEED_1', 'S')]
+    FILE_NAME_KEYS = [('DISPATCHERS', 'D'), ('DISPATCH_RATE', 'R'), ('FAILURE_TIMEOUT', 'T'), ('MAX_SPEED', 'S')]
     DIR_KEYS = ['K']
     SERVER_TIMEOUT = 120
     PROCESS_JOIN_TIMEOUT = 120

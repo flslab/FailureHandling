2a8e702c3561070538da224124d6aced40665886
Shuqin <shuqinzhu37@gmail.com>
2023-08-14 23:35:27 -0700

    fixed float in naming function

---
+++ experiments/config10.py
@@ -0,0 +1,26 @@
+class Config:
+    INITIAL_RANGE = 2000
+    MAX_RANGE = 2000
+    DROP_PROB_SENDER = 0
+    DROP_PROB_RECEIVER = 0
+    DEAD_RECKONING_ANGLE = 0
+    FAILURE_TIMEOUT = 60
+    FAILURE_PROB = 0
+    ACCELERATION = 10
+    DECELERATION = 10
+    MAX_SPEED = 30
+    DISPLAY_CELL_SIZE = 0.05
+    BUSY_WAITING = False
+    DURATION = 60 * 10
+    K = 3
+    SHAPE = 'chess'
+    RESULTS_PATH = '/proj/nova-PG0/shuqin/results'
+    DEBUG = False
+    FILE_NAME_KEYS = [('DISPATCHERS', 'D'), ('DISPATCH_RATE', 'R'), ('FAILURE_TIMEOUT', 'T')]
+    DIR_KEYS = ['K']
+    SERVER_TIMEOUT = 120
+    PROCESS_JOIN_TIMEOUT = 120
+    DISPATCHERS = 1
+    DISPATCH_RATE = 'inf'
+    MULTICAST = False
+    INPUT = 'chess_K3'

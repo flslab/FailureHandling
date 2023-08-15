34ea5aa6263c59b96a552d09b332f617dfd38336
Shuqin <shuqinzhu37@gmail.com>
2023-08-14 22:42:39 -0700

    change hostname and node num

--- cloudlab_vars.sh
+++ cloudlab_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 idx=1
-num_of_total_servers=15
-N=14
-HOSTNAME="Shuqin-163522.nova-PG0.clemson.cloudlab.us"
+num_of_total_servers=16
+N=15
+HOSTNAME="FailureHandling.nova-PG0.clemson.cloudlab.us"
 USERNAME="Shuqin"

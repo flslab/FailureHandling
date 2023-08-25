b95c0ff4efc9675c14a14f098b05b2d4fdbb5eb2
Shuqin <shuqinzhu37@gmail.com>
2023-08-24 18:42:48 -0700

    ready for cloudlab

--- gen_conf.py
+++ gen_conf.py
@@ -44,11 +44,11 @@ general_props = [
     {
         "keys": ["K", "INPUT"],
         "values": [
-            {"K": "0", "INPUT": "'chess_K3'"},
-            # {"K": "3", "INPUT": "'chess_K3'"},
+            # {"K": "0", "INPUT": "'chess_K3'"},
+            {"K": "3", "INPUT": "'chess_K3'"},
             # {"K": "5", "INPUT": "'chess_K5'"},
-            # {"K": "10", "INPUT": "'chess_K10'"},
-            # {"K": "20", "INPUT": "'chess_K20'"},
+            {"K": "10", "INPUT": "'chess_K10'"},
+            {"K": "20", "INPUT": "'chess_K20'"},
         ]
     },
     {

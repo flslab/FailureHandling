18aa2c70e22595a6e1154b6458efccebde246041
Shuqin <shuqinzhu37@gmail.com>
2023-08-29 18:22:28 -0700

    update

--- primary.py
+++ primary.py
@@ -592,7 +592,6 @@ if __name__ == '__main__':
         name = sys.argv[2]
 
     primary_node = PrimaryNode(N, name)
-
     primary_node.start_termination_timer()
     primary_node.start_experiment()
     # primary_node.stop_experiment()

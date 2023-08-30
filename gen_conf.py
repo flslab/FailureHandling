c16cc86e5df2455426949a6f2665c87b4f845f10
Shuqin <shuqinzhu37@gmail.com>
2023-08-30 07:52:44 -0700

    fix directory

--- gen_conf.py
+++ gen_conf.py
@@ -39,7 +39,7 @@ def_general_conf = {
 general_props = [
     {
         "keys": ["DISPATCH_RATE"],
-        "values": ["100", "'inf'"]
+        "values": ["100", "'inf'"],
         # "values": ["10", "50", "100", "'inf'"]
         # "values": ["10", "50"]
     },

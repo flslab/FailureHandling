137f02b832cbb720b81142b5992e122c0b35b486
Shuqin <shuqinzhu37@gmail.com>
2023-09-04 23:49:11 -0700

    swtich to dragon

--- gen_conf.py
+++ gen_conf.py
@@ -17,7 +17,7 @@ def_general_conf = {
     "BUSY_WAITING": "False",
     "DURATION": "60 * 30",
     "K": "3",
-    "SHAPE": "'chess'",
+    "SHAPE": "'dragon'",
     "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
     # "RESULTS_PATH": "'/Users/shuqinzhu/Desktop/experiments_aug29.nosync'",
     "DEBUG": "False",
@@ -40,19 +40,19 @@ def_general_conf = {
 general_props = [
     {
         "keys": ["DISPATCH_RATE"],
-        "values": ["5"],
-        # "values": ["10", "50", "100", "'inf'"]
+        # "values": ["5"],
+        "values": ["10", "50", "100", "'inf'"]
         # "values": ["10", "50"]
     },
     {
         "keys": ["K", "INPUT", "DISPATCHERS"],
         "values": [
             # {"K": "0", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
-            {"K": "0", "INPUT": "'chess_K3'", "DISPATCHERS": "3"},
+            # {"K": "0", "INPUT": "'chess_K3'", "DISPATCHERS": "3"},
             # {"K": "3", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
             # {"K": "5", "INPUT": "'chess_K5'"},
-            # {"K": "10", "INPUT": "'chess_K10'", "DISPATCHERS": "1"},
-            # {"K": "20", "INPUT": "'chess_K20'", "DISPATCHERS": "1"},
+            {"K": "10", "INPUT": "'chess_K10'", "DISPATCHERS": "1"},
+            {"K": "20", "INPUT": "'chess_K20'", "DISPATCHERS": "1"},
         ]
     },
     # {
@@ -72,8 +72,8 @@ general_props = [
     # },
     {
         "keys": ["FAILURE_TIMEOUT"],
-        # "values": ["30", "60"]
-        "values": ["60"]
+        "values": ["30", "60"]
+        # "values": ["60"]
         # "values": ["1", "3", "6", "30", "60", "120", "600"]
     },
     {
@@ -86,8 +86,8 @@ general_props = [
     },
     {
         "keys": ["DISPATCHER_ASSIGN_POLICY"],
-        "values": ["'ShortestDistance'", "'PoD'", "'RR'", "'Random'"]
-        # "values": ["'Random'"]
+        # "values": ["'ShortestDistance'", "'PoD'", "'RR'", "'Random'"]
+        "values": ["'ShortestDistance'"]
     },
     # {
     #     "keys": ["SANITY_TEST", "DISPATCHERS"],

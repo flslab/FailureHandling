4bc2d4d82bfb7a92d5bdb6432e6b6b44fa45d7a5
Shuqin <shuqinzhu37@gmail.com>
2023-09-05 23:40:37 -0700

    change filename

--- gen_conf.py
+++ gen_conf.py
@@ -49,10 +49,10 @@ general_props = [
         "values": [
             # {"K": "0", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
             # {"K": "0", "INPUT": "'chess_K3'", "DISPATCHERS": "3"},
-            {"K": "3", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
+            # {"K": "3", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
             # # {"K": "5", "INPUT": "'chess_K5'"},
-            {"K": "10", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
-            {"K": "20", "INPUT": "'chess_K3'", "DISPATCHERS": "1"},
+            {"K": "10", "INPUT": "'chess_K10'", "DISPATCHERS": "1"},
+            {"K": "20", "INPUT": "'chess_K20'", "DISPATCHERS": "1"},
         ]
     },
     # {

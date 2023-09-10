b833bfbbe8155dea8bef9f4bde4657f0238757df
Shuqin <shuqinzhu37@gmail.com>
2023-09-10 12:17:48 -0700

    debug

--- gen_conf_cluster.sh
+++ gen_conf_cluster.sh
@@ -6,6 +6,7 @@ python3 gen_conf.py
 
 for (( i=1; i<num_of_total_servers; i++ )); do
     server_addr=${USERNAME}@node-$i.${HOSTNAME}
-    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "cd FailureHandling && python3 gen_conf.py" &
-    echo "Generate Config"
+    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "cd FailureHandling && sudo python3 gen_conf.py" &
 done
+
+echo "Generate Config"

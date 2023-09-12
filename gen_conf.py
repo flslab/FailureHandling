2344f89961eba8ad67bf48948027f1fbccb67767
Shuqin <shuqinzhu37@gmail.com>
2023-09-11 18:41:28 -0700

    change name

--- gen_conf.py
+++ gen_conf.py
@@ -187,7 +187,7 @@ general_props = [
             {"SHAPE": "'racecar'", "K": "0", "INPUT": "'racecar_K3'", "DISPATCHERS": "1", "DISPATCH_RATE": "350",
              "FAILURE_TIMEOUT": "30", "MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11",
              "PRIORITIZE_ILLUMINATING_FLS": "False", "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
-             "DISPLAY_CELL_SIZE": "0.016", "NAME": "'child'"},
+             "DISPLAY_CELL_SIZE": "0.016", "NAME": "'child2'"},
 
             {"SHAPE": "'racecar'", "K": "0", "INPUT": "'racecar_K3'", "DISPATCHERS": "1", "DISPATCH_RATE": "350",
              "FAILURE_TIMEOUT": "30", "MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11",

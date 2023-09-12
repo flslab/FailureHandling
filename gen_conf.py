c4c87ae7dd9980611a9f1fa7423df01ea5f5bcb6
Shuqin <shuqinzhu37@gmail.com>
2023-09-11 18:17:43 -0700

    pass experiment

--- gen_conf.py
+++ gen_conf.py
@@ -179,10 +179,10 @@ general_props = [
             #  "PRIORITIZE_ILLUMINATING_FLS": "False", "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
             #  "DISPLAY_CELL_SIZE": "0.043", "NAME": "'real2'"},
 
-            {"SHAPE": "'racecar'", "K": "0", "INPUT": "'racecar_K3'", "DISPATCHERS": "1", "DISPATCH_RATE": "350",
-             "FAILURE_TIMEOUT": "30", "MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11",
-             "PRIORITIZE_ILLUMINATING_FLS": "False", "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
-             "DISPLAY_CELL_SIZE": "0.000672", "NAME": "'match2'"},
+            # {"SHAPE": "'racecar'", "K": "0", "INPUT": "'racecar_K3'", "DISPATCHERS": "1", "DISPATCH_RATE": "350",
+            #  "FAILURE_TIMEOUT": "30", "MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11",
+            #  "PRIORITIZE_ILLUMINATING_FLS": "False", "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
+            #  "DISPLAY_CELL_SIZE": "0.000672", "NAME": "'match2'"},
 
             {"SHAPE": "'racecar'", "K": "0", "INPUT": "'racecar_K3'", "DISPATCHERS": "1", "DISPATCH_RATE": "350",
              "FAILURE_TIMEOUT": "30", "MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11",

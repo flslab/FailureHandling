5ea5edcc1053cf91d5219dff5f629bf9c686bd23
Shuqin <shuqinzhu37@gmail.com>
2023-09-14 12:05:07 -0700

    change directory

--- gen_conf.py
+++ gen_conf.py
@@ -18,7 +18,7 @@ def_general_conf = {
     "DURATION": "60 * 30",
     "K": "3",
     "SHAPE": "'chess'",
-    "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results_kmeans/'",
+    "RESULTS_PATH": "'/proj/nova-PG0/shuqin/results/'",
     # "RESULTS_PATH": "'/Users/shuqinzhu/Desktop/experiments_aug29.nosync'",
     "DEBUG": "False",
     "FILE_NAME_KEYS": "[('DISPATCHERS', 'D'), ('DISPATCH_RATE', 'R'), ('FAILURE_TIMEOUT', 'T'), ('MAX_SPEED', 'S'),"

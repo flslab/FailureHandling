a567c5cad149588a6baa2b6c7f93be409be3c1ad
hamed <amzhamed@gmail.com>
2023-09-22 16:53:13 -0700

    cleanup deadcode

--- run_all_cluster.sh
+++ run_all_cluster.sh
@@ -3,12 +3,12 @@
 bash gen_conf_cluster.sh
 sleep 10
 
-for i in {0..7}
+for i in {0..15}
 do
 #   for j in {0..1}
 #   do
      sleep 2
-#     echo "$i" "$j"
+     echo "$i"
      bash start_cluster.sh "$i"
 #   done
 done
--- utils/distribution.py
+++ utils/distribution.py
@@ -5,29 +5,19 @@ import matplotlib.pyplot as plt
 
 
 def left_half_exponential(n):
-    if n <= 0:
-        raise ValueError("Input n must be a positive float")
-
-    # Generate a random number from the exponential distribution
-    lambda_param = 1.0 / n
-    x = np.random.exponential(scale=1 / lambda_param)
-
-    # Ensure the generated number is within the range [0, n]
-    while x > n:
-        x = np.random.exponential(scale=1 / lambda_param)
-
-    # Get the left half of the distribution
-    return n - x
+    alpha, beta = 10, 0.8
+    return random.betavariate(alpha, beta) * n
 
 
 if __name__ == '__main__':
     data = []
-    alpha, beta = 5, 1
+    alpha, beta = 10, 0.8
     for i in range(1000000):
         r = random.betavariate(alpha, beta)
         # while x > 60:
         #     x = np.random.exponential(scale=1 / lambda_param)
 
-        data.append(r*60)
-    count, bins, ignored = plt.hist(data, bins=1000, density=False)
+        data.append(r * 60)
+    count, bins, ignored = plt.hist(data, bins=100, density=False)
+    print(np.mean(data))
     plt.show()

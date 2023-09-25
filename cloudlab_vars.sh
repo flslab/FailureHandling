803a42a4af787fa372a755bdbc39eba0d72a03f2
hamed <amzhamed@gmail.com>
2023-09-24 23:33:30 -0700

    experiment sk

--- cloudlab_vars.sh
+++ cloudlab_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 idx=1
-num_of_total_servers=13
-N=12
-HOSTNAME="dragon.nova-PG0.wisc.cloudlab.us"
+num_of_total_servers=9
+N=8
+HOSTNAME="hamedamz-168459.nova-PG0.clemson.cloudlab.us"
 USERNAME="hamedamz"

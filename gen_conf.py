c78264412a58ae63073d8dc34e1e216ab35db16c
Shuqin <shuqinzhu37@gmail.com>
2023-10-01 13:18:46 -0700

    T120 S0.3

--- gen_conf.py
+++ gen_conf.py
@@ -77,8 +77,8 @@ general_props = [
         "keys": ["K", "INPUT", "SHAPE", "FAILURE_TIMEOUT", "DURATION"],
         "values": [
 
-            {"K": "0", "INPUT": "'skateboard_G3'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "60", "DURATION": "60 * 5"},
-            {"K": "3", "INPUT": "'skateboard_G3'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "60", "DURATION": "60 * 5"},
+            {"K": "0", "INPUT": "'skateboard_G3'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "120", "DURATION": "60 * 10"},
+            {"K": "3", "INPUT": "'skateboard_G3'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "120", "DURATION": "60 * 10"},
             # {"K": "5", "INPUT": "'skateboard_G5'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "900", "DURATION": "60 * 60"},
             # {"K": "10", "INPUT": "'skateboard_G10'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "900", "DURATION": "60 * 60"},
             # {"K": "15", "INPUT": "'skateboard_G15'", "SHAPE": "'skateboard'", "FAILURE_TIMEOUT": "900", "DURATION": "60 * 60"},
@@ -110,7 +110,7 @@ general_props = [
     {
         "keys": ["MAX_SPEED", "ACCELERATION", "DECELERATION"],
         "values": [
-            {"MAX_SPEED": "1", "ACCELERATION": "1", "DECELERATION": "1"},
+            {"MAX_SPEED": "0.3", "ACCELERATION": "0.3", "DECELERATION": "0.3"},
             # {"MAX_SPEED": "6.11", "ACCELERATION": "6.11", "DECELERATION": "6.11"},
             # {"MAX_SPEED": "30", "ACCELERATION": "30", "DECELERATION": "30"},
             # {"MAX_SPEED": "66.67", "ACCELERATION": "66.67", "DECELERATION": "66.67"},

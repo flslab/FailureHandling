61b4a741de003cc91b491a390e0c8242e054403c
Shuqin <shuqinzhu37@gmail.com>
2023-10-22 12:41:15 -0700

    update

--- utils/obstructing_FLSs.py
+++ utils/obstructing_FLSs.py
@@ -357,9 +357,9 @@ if __name__ == "__main__":
 
         for shape in ["skateboard", "dragon", "hat"]:
             for k in [3, 20]:
-                # calculate_obstructing(file_folder, meta_dir, illum_to_disp_ratio, k, shape)
-                p_list.append(mp.Process(target=calculate_obstructing, args=(file_folder, meta_dir, illum_to_disp_ratio, k, shape)))
+                calculate_obstructing(file_folder, meta_dir, illum_to_disp_ratio, k, shape)
+                # p_list.append(mp.Process(target=calculate_obstructing, args=(file_folder, meta_dir, illum_to_disp_ratio, k, shape)))
 
-    for p in p_list:
-        print(p)
-        p.start()
+    # for p in p_list:
+    #     print(p)
+    #     p.start()
